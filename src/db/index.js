71ddf9363730586f82a9f3c7e0f7feaaf0f0b867
Rajnikantbhay <rsusishere@gmail.com>
2024-05-24 22:41:22 +0530

    fixed database URI

--- src/db/index.js
+++ src/db/index.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
+  const uri = `${process.env.MONGODB_URI}/${DB_NAME}`;
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
+      `${process.env.MONGODB_URI}`
     );
+    console.log(uri);
     console.log(
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
     );
