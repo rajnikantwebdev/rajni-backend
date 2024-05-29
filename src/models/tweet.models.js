7afd7c772ec20c8a2232a1f4005642e0592bb1b3
Rajnikantbhay <rsusishere@gmail.com>
2024-05-29 07:11:38 +0530

    Created comment, like, playlist and tweet models

---
+++ src/models/tweet.models.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+const tweetSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Tweet = model("Tween", tweetSchema);
